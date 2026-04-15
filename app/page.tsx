ab61fe1d7ff554fcf48700087fc606312573fd4c
xianxlb <lixiannong@gmail.com>
2026-04-15 12:51:07 +0800

    increase Antler logo height from 24 to 30

--- app/page.tsx
+++ app/page.tsx
@@ -227,7 +227,7 @@ const COMPANY_LOGOS = [
   { src: "/logos/sanctum.svg",                             alt: "Sanctum",           height: 36 },
   { src: "/logos/jupiter.svg",                             alt: "Jupiter",           height: 26 },
   { src: "/logos/meteora.svg",                             alt: "Meteora",           height: 20 },
-  { src: "/logos/antler.svg",                              alt: "Antler",            height: 24 },
+  { src: "/logos/antler.svg",                              alt: "Antler",            height: 30 },
 ];
 
 function LogoTicker() {
